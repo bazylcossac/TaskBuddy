39f17d782b6469928e463da034220d92b8e38ab8
Michał <102479081+bazylcossac@users.noreply.github.com>
2024-06-14 07:54:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import {authCreateAccWithEmail, authLoginWithEmail, loginWithGogle, loginWithFac
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCzOp-vLE5xMR7IQ4StkmEy-Oa-SkP8oPE",
+    apiKey: "key_here",
     authDomain: "taskbuddy-dac4b.firebaseapp.com",
     projectId: "taskbuddy-dac4b",
     storageBucket: "taskbuddy-dac4b.appspot.com",
